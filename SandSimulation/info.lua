66f6374b7d8c1db724aad326ac270c035d0880fc
Moovintwo <sebastian.s.benz@gmail.com>
2025-05-29 17:06:52 -0700

    Added a sub package folder for testing M2Lib

---
+++ SandSimulation/info.lua
@@ -0,0 +1,9 @@
+return {
+
+    name = "Sand Simulation",
+    version = "0.1",
+    author = "Moovintwo",
+    source = "https://github.com/MoovinTwo/SandSimulation/tree/master/SandSimulation",
+    mainScript = "main"
+
+}
